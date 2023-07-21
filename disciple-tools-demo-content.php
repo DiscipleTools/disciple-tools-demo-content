c4623c09f665d5da7984c01719b98de9c6356f76
corsacca <corsacca@gmail.com>
2023-07-21 14:57:25 +0100

    v0.6.4

--- disciple-tools-demo-content.php
+++ disciple-tools-demo-content.php
@@ -4,7 +4,7 @@
  *Plugin Name: Disciple.Tools - Demo Content
  * Plugin URI: https://github.com/DiscipleTools/disciple-tools-demo-content
  * Description: The demo content plugin is for a quickstart with content to the Disciple.Tools system. It is useful for demonstration and training.
- * Version: 0.6.3
+ * Version: 0.6.4
  * Author URI: https://github.com/DiscipleTools
  *
  * @license GPL-2.0 or later
